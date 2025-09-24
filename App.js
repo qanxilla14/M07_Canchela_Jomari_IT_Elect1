468a9035717ced624b7685fb134e74dfea0366f2
Canchela <qanxilla@gmail.com>
2025-09-24 23:15:35 +0800

    App.js

--- App.js
+++ App.js
@@ -1,37 +1,24 @@
 
-// CounterApp.js
-import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+          //Act5
+import CounterApp from './CounterApp';
 
-export default function CounterApp() {
-  const [count, setCount] = useState(0);
+
+import {SafeAreaView,StyleSheet} from 'react-native';
+import ColorChangerApp from './ColorChangerApp.js';
+import CounterApp from "./CounterApp.js";
+export default function App() {
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Counter App</Text>
-      <Text style={styles.counter}>{count}</Text>
-      <Button title="Increment" onPress={() => setCount(count + 1)} />
-      <View style={{ marginVertical: 5 }} />
-      <Button title="Decrement" onPress={() => setCount(count - 1)} />
-    </View>
-  );
+    
+ <SafeAreaView style={styles.container}>
+<CounterApp/>
+ <ColorChangerApp />
+ </SafeAreaView>
+   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  counter: {
-    fontSize: 36,
-    marginBottom: 20,
-  },
+  container:{
+    flex:1
+  }
 });
-          
